c87f30cc0158748b605554de8f69381c1b5ac99a
fanmingfei <az8641683@163.com>
2021-01-25 13:56:30 +0800

    for plan

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
-import createBackground from './gameObjects/background';
-import createBasketFront from './gameObjects/board/basketFront';
-import createBoard from './gameObjects/board/board';
-import createBall from './gameObjects/ball';
-import createBtn from './gameObjects/btn';
 import resources from './resources';
 
-import { Game, resource } from '@eva/eva.js';
+import { Game, GameObject, resource } from '@eva/eva.js';
 import { RendererSystem } from '@eva/plugin-renderer';
-import { ImgSystem } from '@eva/plugin-renderer-img';
+import { Img, ImgSystem } from '@eva/plugin-renderer-img';
 import { EventSystem } from '@eva/plugin-renderer-event';
 import { SpriteAnimationSystem } from '@eva/plugin-renderer-sprite-animation';
 import { RenderSystem } from '@eva/plugin-renderer-render';
@@ -22,7 +17,7 @@ const game = new Game({
     new RendererSystem({
       canvas: document.querySelector('#canvas'),
       width: 750,
-      height: 1484,
+      height: 1642,
       antialias: true,
     }),
     new ImgSystem(),
@@ -36,41 +31,16 @@ const game = new Game({
 });
 
 game.scene.transform.size.width = 750;
-game.scene.transform.size.height = 1484;
+game.scene.transform.size.height = 1642;
 
-const pos = {
-  x: 500,
-  y: 1100,
-};
-
-const ball = createBall(pos);
-const { basetFront, playAnim } = createBasketFront();
-const btn = createBtn({
-  text: '投球',
-  transform: {
-    position: {
-      x: 0,
-      y: -120,
-    },
-    origin: {
-      x: 0.5,
-      y: 0.5,
-    },
-    anchor: {
-      x: 0.5,
-      y: 1,
-    },
-  },
-  callback: () => {
-    alert('还没做呢～一起来完善吧')
-  },
-});
-
-game.scene.addChild(createBackground());
-game.scene.addChild(createBoard());
-game.scene.addChild(ball);
-game.scene.addChild(basetFront);
-game.scene.addChild(btn);
-
-window.playAnim = playAnim;
-window.game = game;
+// 添加背景
+const bg = new GameObject('bg', {
+  size: {
+    width: 750,
+    height: 1642
+  }
+})
+bg.addComponent(new Img({
+  resource: 'bg'
+}))
+game.scene.addChild(bg)
